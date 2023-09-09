003615930e15abbd8d52b780636581ea0b1c662f
DevFrog92 <yongsu1481@gmail.com>
2023-09-09 20:18:31 +0900

    feat: implement ticket office domain features

---
+++ 01/CH01/src/main/java/theator/Ticket.java
@@ -0,0 +1,9 @@
+package theator;
+
+public class Ticket {
+    private Long fee;
+
+    public Long getFee() {
+        return fee;
+    }
+}
---
+++ 01/CH01/src/main/java/theator/TicketSeller.java
@@ -0,0 +1,16 @@
+package theator;
+
+/**
+ * 매표소 직원은 자신이 일 할 매표소를 알고 있어야 한다.
+ */
+public class TicketSeller {
+    private final TicketOffice ticketOffice;
+
+    public TicketSeller(TicketOffice ticketOffice) {
+        this.ticketOffice = ticketOffice;
+    }
+
+    public TicketOffice getTicketOffice() {
+        return ticketOffice;
+    }
+}
