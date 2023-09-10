9151cd80b7b4940faec7ee2b8bb2f2ecf4eb4b3d
DevFrog92 <yongsu1481@gmail.com>
2023-09-10 14:52:12 +0900

    refactor: add comment

--- 01/CH01/src/main/java/theater/Audience.java
+++ 01/CH01/src/main/java/theater/Audience.java
@@ -1,7 +1,7 @@
 package theater;
 
 public class Audience {
-    private Bag bag;
+    private final Bag bag;
 
     public Audience(Bag bag) {
         this.bag = bag;
--- 01/CH01/src/main/java/theater/Theater.java
+++ 01/CH01/src/main/java/theater/Theater.java
@@ -7,14 +7,25 @@ public class Theater {
         this.ticketSeller = ticketSeller;
     }
 
+    /**
+     * Theater 에서 bag, ticket office 에 의존하고 있다.
+     * bag 또는 ticket seller 의 내부 로직이 변경되면 enter 로직도 함께 변경되어야 한다.
+     */
+
     public void enter(Audience audience) {
         if(audience.getBag().hasInvitation()) {
+            // 의사소통
             Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+            // 변경
             audience.getBag().setTicket(ticket);
         } else {
+            // 의사소통
             Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+            // 변경
             audience.getBag().minusAmount(ticket.getFee());
+            // 의사소통
             ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
+            // 변경
             audience.getBag().setTicket(ticket);
         }
     }
